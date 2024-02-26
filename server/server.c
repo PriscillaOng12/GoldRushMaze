48e6cf05256a30d4c3df943541b1d86656375af4
Edward H. Kim-3 <f0071b6@plank.thayer.dartmouth.edu>
2024-02-26 01:01:12 -0500

    finished being able to get the first word of a message and some of play functionality

--- server/server.c
+++ server/server.c
@@ -4,22 +4,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include "./support/log.h"
 #include "./support/message.h"
 
 static bool parseArgs(const int argc, char* argv[]);
 static bool handleMessage(void* arg, const addr_t from, const char* message);
-static bool sendResponse(void* arg);
 
 int main(const int argc, const char** argv) {
 
-  if (parseArgs(argc, argv) != NULL) {
-    
+  //validate args
+  if (parseArgs(argc, argv) == NULL) {
+    return 1;
   }
 
+  //initialize message module
+  if (message_init(NULL) == 0) {
+    return 1;
+  }
+
+  //FIGURE OUT HOW TO GET ADDRESS INFORMATION
+  char* serverHost;
+  char* serverPort;
+  addr_t* server;
+  if (!message_setAddr(serverHost, serverPort, &server)) {
+    fprintf(stderr, "can't form address from %s %s\n", serverHost, serverPort);
+    return 4; // bad hostname/port
+  }
+
+  message_loop(&server, 0, NULL, NULL, handleMessage);
+
+  message_done();
   
-
-
   return 0;
 }
 
@@ -55,9 +72,30 @@ static bool parseArgs(const int argc, char* argv[]) {
 }
 
 static bool handleMessage(void* arg, const addr_t from, const char* message) {
+  //get first word from message
+  char* firstWord;
+  int firstSpace = 0;
+  while (!isspace(message[i])) {
+    firstSpace++;
+  }
+  //amount of bytes needed for first word of message + 1 for null terminating
+  firstWord = malloc(firstSpace + 1);
+  strncpy(firstWord, message, firstSpace);
+  firstWord[firstSpace] = '\0';
 
-}
+  //if first word is PLAY
+  if (strcmp(firstWord, "PLAY") == 0) {
+    //get real_name, i.e. rest of message
+    char* real_name = malloc(strlen(message) - firstSpace - 1);
+    strcopy(real_name, message[firstSpace + 1]);
+    //if name is empty, send error message
+    if (strcmp(real_name, "") == 0) {
+      message_send(from, "name must be provided");
+    }
 
-static bool sendResponse(void* arg) {
+
+
+  }
+
 
 }
