0c090049a63737b581a6d9e0d779110c9994b5f7
Karun Ram <karun.ram.25@dartmouth.edu>
2024-03-02 13:11:19 -0500

    added player activity bool, fixed send_state

--- server/player.h
+++ server/player.h
@@ -23,7 +23,7 @@ void player_collect_gold(player_t* player, grid_t* grid, int gold_x, int gold_y)
 
 bool player_move(player_t* player, grid_t* grid, int dx, int dy);
 
-void player_quit(player_t* player, grid_t* grid);
+void player_quit(player_t* player);
 
 char* player_get_name(player_t* player);
 
@@ -38,5 +38,7 @@ int player_get_purse(player_t* player);
 int** player_get_visibility(player_t* player);
 
 void player_set_visibility(player_t* player, int x, int y, int val);
+
+bool player_get_isactive(player_t* player);
 
 #endif //__PLAYER_H
