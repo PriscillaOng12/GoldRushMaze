cbfe544738887b902c0a0b6d97e9eb4c2a393d95
Edward H. Kim-3 <f0071b6@plank.thayer.dartmouth.edu>
2024-03-02 18:26:02 -0500

    fixed server

--- server/server.c
+++ server/server.c
@@ -13,14 +13,13 @@
 #include "file.h"
 #include "grid.h"
 #include "player.h"
+#include "spectator.h"
 
 static bool parseArgs(const int argc, const char** argv);
 static bool handleMessage(void* arg, const addr_t from, const char* message);
 // grid_t* gameGrid;
 
 int main(const int argc, const char** argv) {
-  printf("hello");
-
   //validate args
   if (!parseArgs(argc, argv)) {
     return 1;
@@ -35,21 +34,10 @@ int main(const int argc, const char** argv) {
 
   printf("Server port is:%d", serverPort);
 
-  // //FIGURE OUT HOW TO GET ADDRESS INFORMATION
-  // char* serverHost = "plank.thayer.dartmouth.edu";
-  // char* serverPortString;
-  // sprintf(serverPortString, "%d", serverPort);
-  // addr_t server;
-  // if (!message_setAddr(serverHost, serverPort, &server)) {
-  //   fprintf(stderr, "can't form address from %s %s\n", serverHost, serverPort);
-  //   return 4; // bad hostname/port
-  // }
-
-
-  // char* mapPath = malloc(strlen(argv[1]) + 9);
-  // strcpy(mapPath, "../maps/");
-  // strcat(mapPath, argv[1]);
-  FILE* fp = fopen(argv[1], "r");
+  char* mapPath = malloc(strlen(argv[1]) + 9);
+  strcpy(mapPath, "../maps/");
+  strcat(mapPath, argv[1]);
+  FILE* fp = fopen(mapPath, "r");
   grid_t* gameGrid = grid_load(fp);
   fclose(fp);
   // free(mapPath);
@@ -146,12 +134,13 @@ static bool handleMessage(void* arg, const addr_t from, const char* message) {
     
     //spawns players
     grid_spawn_player(gameGrid, &from, real_name_truncated);
+    //get updated player count and player list
+    int playerCount = grid_getplayercount(gameGrid);
+    player_t** playerList = grid_getplayers(gameGrid);
     //send OK message with player symbol
-    char playerCharacter = (char)(playerCount + 1);
     char* messageToSend = malloc(128);
-    strcpy(messageToSend, "OK ");
-    strcat(messageToSend, &playerCharacter); //MAKING CHAR INTO CHAR*, problem
-    messageToSend[playerCount] = '\0';
+    char playerCharacter = (char) (64 + playerCount);
+    sprintf(messageToSend, "OK %c", playerCharacter);
     message_send(from, messageToSend);
     free(messageToSend);
 
@@ -170,17 +159,29 @@ static bool handleMessage(void* arg, const addr_t from, const char* message) {
     free(messageToSend);
 
     //send initial GOLD message
-    char* remainingGold = malloc(sizeof(int));
-    sprintf(remainingGold, "%d", grid_getnuggetcount(gameGrid));
     messageToSend = malloc(128);
-    strcpy(messageToSend, "GOLD 0 0 ");
-    strcat(messageToSend, remainingGold);
-    messageToSend[9 + strlen(remainingGold)] = '\0';
+    sprintf(messageToSend, "GOLD 0 0 %d", grid_getnuggetcount(gameGrid));
+    // messageToSend[9 + strlen(remainingGold)] = '\0';
     message_send(from, messageToSend);
     free(messageToSend);
 
-    //send DISPLAY message
-    grid_send_state(gameGrid, playerList[playerCount - 1]); //WILL THIS WORK???
+    //send DISPLAY message to all players and spectator
+    for (int i = 0; i < playerCount; i++) {
+      if (player_get_isactive(playerList[i])) {
+        //if player is active, send them updated DISPLAY
+        messageToSend = malloc(128);
+        messageToSend = grid_send_state(gameGrid, playerList[i]);
+        message_send(*player_get_addr(playerList[i]), messageToSend);
+        free(message);
+      }
+    }
+    //if there is spectator, send them updated DISPLAY
+    if (grid_getspectatorCount(gameGrid) == 1) 
+    {
+      messageToSend = malloc(128);
+      messageToSend = grid_send_state_spectator(gameGrid);
+      message_send(*spectator_get_addr(grid_getspectator), messageToSend);
+    }
     return false;
   }
   else if (strcmp(firstWord, "KEY") == 0) {
@@ -194,9 +195,9 @@ static bool handleMessage(void* arg, const addr_t from, const char* message) {
       }
     }
     
-    char* keyStroke = malloc(strlen(message) - 5);
-    strcpy(keyStroke, &message[4]); //DOES THIS WORK
-    if (strcmp(keyStroke, "h") == 0) {
+    char* keyStroke = malloc(128);
+    strcpy(keyStroke, message + 4); //DOES THIS WORK
+    if (strcmp(keyStroke, "h") == 0) { //CAPITAL CHARACTER FIX, CHECK IF SPECTATOR
       player_moveto(matchingPlayer, -1, 0);
     }
     else if (strcmp(keyStroke, "l") == 0) {
@@ -221,11 +222,16 @@ static bool handleMessage(void* arg, const addr_t from, const char* message) {
       player_moveto(matchingPlayer, 1, -1);
     }
     else if (strcmp(keyStroke, "Q") == 0) {
+      free(keyStroke);
       if (matchingPlayer == NULL) {
         message_send(from, "QUIT Thanks for watching!");
         return false;
       }
       message_send(from, "QUIT Thanks for playing!");
+      return false;
+    }
+    else {
+      message_send(from, "ERROR unknown keystroke");
       return false;
     }
     free(keyStroke);
@@ -234,39 +240,58 @@ static bool handleMessage(void* arg, const addr_t from, const char* message) {
       grid_game_over(gameGrid);
       return true;
     }
-    else if (strcmp(firstWord, "SPECTATE") == 0) {
-      //spawns spectator
-      grid_spawn_spectator(gameGrid, &from); //MAKE THIS
-
-      //send initial GRID message with grid information
-      char* nrows = NULL;
-      sprintf(nrows, "%d", grid_getnrows(gameGrid));
-      char* ncols = NULL;
-      sprintf(ncols, "%d", grid_getncols(gameGrid));
-      char* messageToSend = malloc(5 + strlen(nrows) + 1 + strlen(ncols) + 1);
-      strcpy(messageToSend, "GRID ");
-      strcat(messageToSend, nrows);
-      strcat(messageToSend, " ");
-      strcat(messageToSend, ncols);
-      messageToSend[5 + strlen(nrows) + 1 + strlen(ncols)] = '\0';
-      message_send(from, messageToSend);
-      free(messageToSend);
-      messageToSend = NULL;
-
-      //send initial GOLD message
-      char* remainingGold = NULL;
-      sprintf(remainingGold, "%d", **grid_getnuggets(gameGrid));
-      messageToSend = realloc(messageToSend, 9 + strlen(remainingGold) + 1);
-      strcpy(messageToSend, "GOLD 0 0 ");
-      strcat(messageToSend, remainingGold);
-      messageToSend[9 + strlen(remainingGold)] = '\0';
-      message_send(from, messageToSend);
-      free(messageToSend);
-
-      //send DISPLAY message
-      grid_send_state_spectator(gameGrid, grid_getspectator(gameGrid)); //THIS METHOD MUST BE MADE
-      return false;
+    //send DISPLAY message to all players and spectator
+    for (int i = 0; i < playerCount; i++) {
+      if (player_get_isactive(playerList[i])) {
+        //if player is active, send them updated DISPLAY
+        char* messageToSend = malloc(128);
+        messageToSend = grid_send_state(gameGrid, playerList[i]);
+        message_send(*player_get_addr(playerList[i]), messageToSend);
+        free(message);
+      }
     }
+    //if there is spectator, send them updated DISPLAY
+    if (grid_getspectatorCount(gameGrid) == 1) 
+    {
+      char* messageToSend = malloc(128);
+      messageToSend = grid_send_state_spectator(gameGrid);
+      message_send(*spectator_get_addr(grid_getspectator), messageToSend);
+    }
+    return false;
   }
-  return false;
+  else if (strcmp(firstWord, "SPECTATE") == 0) {
+    //spawns spectator
+    grid_spawn_spectator(gameGrid, spectator_new(&from)); //MAKE THIS
+
+    //send initial GRID message with grid information
+    char* nrows = malloc(sizeof(int));
+    sprintf(nrows, "%d", grid_getnrows(gameGrid));//problem
+    char* ncols = malloc(sizeof(int));
+    sprintf(ncols, "%d", grid_getncols(gameGrid));
+    char* messageToSend = malloc(128);
+    strcpy(messageToSend, "GRID ");
+    strcat(messageToSend, nrows);
+    strcat(messageToSend, " ");
+    strcat(messageToSend, ncols);
+    messageToSend[5 + strlen(nrows) + 1 + strlen(ncols)] = '\0';
+    message_send(from, messageToSend);
+    free(messageToSend);
+
+    //send initial GOLD message
+    messageToSend = malloc(128);
+    sprintf(messageToSend, "GOLD 0 0 %d", grid_getnuggetcount(gameGrid));
+    message_send(from, messageToSend);
+    free(messageToSend);
+
+    //send DISPLAY message
+    messageToSend = grid_send_state_spectator(gameGrid);
+    message_send(from, messageToSend);
+    free(messageToSend);
+
+    return false;
+  }
+  else {
+    message_send(from, "invalid message");
+    return true;
+  }
 }
